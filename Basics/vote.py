1f9b18630ea3c3db78e1f5e32d79b51a165be038
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-24 22:24:01 +0530

    Add files via upload

---
+++ Basics/vote.py
@@ -0,0 +1,16 @@
+age=int(input("enter age: "))
+nation=input("enter nationality: ")
+
+if(age>=18):
+    
+    if(nation == "indian"):
+        print("eligble for vote")
+    elif(nation == "INDIAN"):
+        print("eligble for vote")
+    else:
+        print("sorry !! you are from different country")
+        print("not eligble for vote")
+
+else:
+    print("not eligble for vote \n age must be 18 or above!!")
+s
