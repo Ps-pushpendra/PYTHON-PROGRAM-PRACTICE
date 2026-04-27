4b601748eca725f66915eb3f0fa0667fb6c0051e
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-27 23:14:00 +0530

    Update seats.py

--- List/seats.py
+++ List/seats.py
@@ -1,3 +1,9 @@
+# python program to check the availability of seats as the seats given in the from of list.
+
+# 1 = booked , 0 = empty seat
+
+# seat arrangement
+
 seats = [
     [1,0,0,1,0],
     [0,1,1,0,0],
@@ -6,11 +12,11 @@ seats = [
     [1,1,0,0,1]
     ]
     
-booked = empty = 0
+booked = empty = 0  
     
-for i in range(len(seats)):
+for i in range(len(seats)): 
     for j in range(len(seats[i])):
-        print(seats[i][j],end=" ")
+        print(seats[i][j],end=" ") # here end is for apace between the seats 
     
         if (seats[i][j]==1):
            booked +=1
