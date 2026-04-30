5739f30df43abc4c4fef6e4d9e75e3abc1d8c328
Pushpendra Singh <pushpendrasinght2007@gmail.com>
2026-04-30 23:02:44 +0530

    Update store.py

--- store.py
+++ store.py
@@ -1,7 +1,9 @@
+# program to calculate the total revenue of store.
+
 orders = [
-    [100,200,150],
-    [300,250],
-    [400,100,50],
+    [100,200,150], #coustmer1  expences
+    [300,250],     #coustmer2  expences
+    [400,100,50],  #coustmer3  expences
     ]
 
 total_revenue =0
@@ -12,7 +14,7 @@ for i in range(len(orders)):
        ct += orders[i][j]
        
     print("coustmer",i+1,"expense=",ct)  
-    total_revenue += ct
+    total_revenue += ct                  # calcutating total revenue of store
 print()    
     
     
